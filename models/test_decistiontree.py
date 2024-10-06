d32c1edd4b4843db882e4adf936c8683336f525e
Malc <115590219+Malckub@users.noreply.github.com>
2024-10-06 20:53:31 +0700

    😊delete new_data ที่ไม่ได้ใช้

--- models/test_decistiontree.py
+++ models/test_decistiontree.py
@@ -30,19 +30,6 @@ new_data = pd.DataFrame({
     'sulphates': [sulphate],
     'alcohol': [alc],
 })
-new_data = pd.DataFrame({
-    'fixed acidity': [7.4],
-    'volatile acidity': [0.7],
-    'citric acid': [0],
-    'residual sugar': [1.9],
-    'chlorides': [0.076],
-    'free sulfur dioxide': [11],
-    'total sulfur dioxide': [34],
-    'density': [0.9978],
-    'pH': [3.51],
-    'sulphates': [0.56],
-    'alcohol': [9.4],
-    # เพิ่มฟีเจอร์ตามที่โมเดลใช้ในการฝึก
-})
+
 prediction = loaded_model.predict(new_data)
 print("Prediction:",prediction)
